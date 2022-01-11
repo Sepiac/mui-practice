da099606694e3ac255930d9759689d013161dff5
Dane Russell <danerussell4@gmail.com>
2022-01-11 12:08:09 -0500

    feat(MainMenu): add drawer

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -1,22 +1,41 @@
-import React from 'react'
-import { AppBar, Toolbar, IconButton, Typography } from '@mui/material'
+import React, { useState } from 'react'
+import { AppBar, Toolbar, IconButton, Typography, Drawer, List, ListItem, ListItemIcon, Divider } from '@mui/material'
 import MenuIcon from '@mui/icons-material/Menu';
 import { SepiacIcon } from '../icons'
+import EmojiPeopleIcon from '@mui/icons-material/EmojiPeople';
+import FaceIcon from '@mui/icons-material/Face';
 
 export const MainMenu = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false)
   return (
     <div>
       <AppBar position="static">
         <Toolbar variant="dense">
-          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }} onClick={() => setIsDrawerOpen(!isDrawerOpen)}>
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" color="inherit" component="div">
-            <SepiacIcon style={{display: 'inline-block', verticalAlign: 'middle', width: '2rem', marginRight: '1rem'}}/>
+            <SepiacIcon style={{ display: 'inline-block', verticalAlign: 'middle', width: '2rem', marginRight: '1rem' }} />
             MUI Practice App
           </Typography>
         </Toolbar>
       </AppBar>
+
+      <Drawer
+        anchor="left"
+        open={isDrawerOpen}
+        onClose={() => setIsDrawerOpen(false)}
+      >
+        <List>
+          <ListItem button >
+            <ListItemIcon><EmojiPeopleIcon /> Hi new friend!</ListItemIcon>
+          </ListItem>
+          <Divider />
+          <ListItem button >
+            <ListItemIcon><FaceIcon />Hello</ListItemIcon>
+          </ListItem>
+        </List>
+      </Drawer>
     </div>
   )
 }
